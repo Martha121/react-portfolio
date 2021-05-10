d0fb6176c53c6e146b8a2044869744973cca94e3
MGcommits <marthagamez211@gmail.com>
2021-05-09 20:05:37 -0500

    Create base code

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
+import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
+
+
+
+function App() {
+
+  const [contactSelected, setContactSelected] = useState(false);
+
+  const [categories] = useState([
+    {
+      name: 'portfolio',
+      description: 'Photos of grocery stores, food trucks, and other commercial projects',
+    },
+    { name: 'resume', description: 'Portraits of people in my life' },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+  return (
+    <div>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav>
+      <main>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
+      </main>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
