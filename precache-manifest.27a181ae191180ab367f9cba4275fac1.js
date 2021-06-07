8e633f944f3d37669b744a3880bd1fed6423601c
MGcommits <marthagamez211@gmail.com>
2021-06-06 19:26:14 -0500

    Updates

---
+++ precache-manifest.27a181ae191180ab367f9cba4275fac1.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "429aa0b1f5399e011da22f6dd203f8e0",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "d94867e87214547e89da",
+    "url": "/react-portfolio/static/css/2.b8ba00a2.chunk.css"
+  },
+  {
+    "revision": "2f21984f80ee6846d6f4",
+    "url": "/react-portfolio/static/css/main.08dec792.chunk.css"
+  },
+  {
+    "revision": "d94867e87214547e89da",
+    "url": "/react-portfolio/static/js/2.b774c3a1.chunk.js"
+  },
+  {
+    "revision": "1b9cc396521eecb2045c787139670fba",
+    "url": "/react-portfolio/static/js/2.b774c3a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f21984f80ee6846d6f4",
+    "url": "/react-portfolio/static/js/main.85e8d10f.chunk.js"
+  },
+  {
+    "revision": "a05a49d9351ba3961537",
+    "url": "/react-portfolio/static/js/runtime-main.bc59954b.js"
+  },
+  {
+    "revision": "b0bc1498ca9d0c07f77e5c2ba9f59478",
+    "url": "/react-portfolio/static/media/Martha-resume.b0bc1498.PNG"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/react-portfolio/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/react-portfolio/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "a3743f22aeb1be9736890db951643cbf",
+    "url": "/react-portfolio/static/media/chooseandchill.a3743f22.png"
+  },
+  {
+    "revision": "500afd5347c37f596f3fe7e2168ad527",
+    "url": "/react-portfolio/static/media/confeastador.500afd53.PNG"
+  },
+  {
+    "revision": "099a9556e1a63ece24f8a99859c94c7d",
+    "url": "/react-portfolio/static/media/fa-brands-400.099a9556.woff"
+  },
+  {
+    "revision": "30cc681d4487d2f561035ba24a68c629",
+    "url": "/react-portfolio/static/media/fa-brands-400.30cc681d.eot"
+  },
+  {
+    "revision": "3b89dd103490708d19a95adcae52210e",
+    "url": "/react-portfolio/static/media/fa-brands-400.3b89dd10.ttf"
+  },
+  {
+    "revision": "ba7ed552362f64d30f6d844974d89114",
+    "url": "/react-portfolio/static/media/fa-brands-400.ba7ed552.svg"
+  },
+  {
+    "revision": "f7307680c7fe85959f3ecf122493ea7d",
+    "url": "/react-portfolio/static/media/fa-brands-400.f7307680.woff2"
+  },
+  {
+    "revision": "0bb428459c8ecfa61b22a03def1706e6",
+    "url": "/react-portfolio/static/media/fa-regular-400.0bb42845.svg"
+  },
+  {
+    "revision": "1f77739ca9ff2188b539c36f30ffa2be",
+    "url": "/react-portfolio/static/media/fa-regular-400.1f77739c.ttf"
+  },
+  {
+    "revision": "7124eb50fc8227c78269f2d995637ff5",
+    "url": "/react-portfolio/static/media/fa-regular-400.7124eb50.woff"
+  },
+  {
+    "revision": "7630483dd4b0c48639d2ac54a894b450",
+    "url": "/react-portfolio/static/media/fa-regular-400.7630483d.eot"
+  },
+  {
+    "revision": "f0f8230116992e521526097a28f54066",
+    "url": "/react-portfolio/static/media/fa-regular-400.f0f82301.woff2"
+  },
+  {
+    "revision": "1042e8ca1ce821518a2d3e7055410839",
+    "url": "/react-portfolio/static/media/fa-solid-900.1042e8ca.eot"
+  },
+  {
+    "revision": "376c1f97f6553dea1ca9b3f9081889bd",
+    "url": "/react-portfolio/static/media/fa-solid-900.376c1f97.svg"
+  },
+  {
+    "revision": "605ed7926cf39a2ad5ec2d1f9d391d3d",
+    "url": "/react-portfolio/static/media/fa-solid-900.605ed792.ttf"
+  },
+  {
+    "revision": "9fe5a17c8ab036d20e6c5ba3fd2ac511",
+    "url": "/react-portfolio/static/media/fa-solid-900.9fe5a17c.woff"
+  },
+  {
+    "revision": "e8a427e15cc502bef99cfd722b37ea98",
+    "url": "/react-portfolio/static/media/fa-solid-900.e8a427e1.woff2"
+  },
+  {
+    "revision": "9f34a5c04466100cf7c493f7c2a4df79",
+    "url": "/react-portfolio/static/media/karoleal1.9f34a5c0.png"
+  },
+  {
+    "revision": "b697a596828e25225698ac28bba3193d",
+    "url": "/react-portfolio/static/media/labmanager.b697a596.png"
+  },
+  {
+    "revision": "00e68da3036b8e268bdc8241e1c5bf01",
+    "url": "/react-portfolio/static/media/martha_inkedin.00e68da3.jpg"
+  },
+  {
+    "revision": "9738b733265c04381346dc0c0a127a7f",
+    "url": "/react-portfolio/static/media/mostachos1.9738b733.png"
+  },
+  {
+    "revision": "e3fd870f1feb1ac338288a07f5dda1c9",
+    "url": "/react-portfolio/static/media/run-buddy1.e3fd870f.png"
+  }
+]);
